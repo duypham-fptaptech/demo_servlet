558b85b6d4e725e6ee740c17197d7f2e737e0e05
duypham-fptaptech <duypdth2009006@fpt.edu.vn>
2022-05-10 08:26:30 +0700

    first commit

---
+++ src/main/java/com/example/demo_tomcat/controller/HelloServlet.java
@@ -0,0 +1,19 @@
+package com.example.demo_tomcat.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet  extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String firstName = req.getParameter("firstName");
+        String lastName = req.getParameter("lastName");
+
+        req.setAttribute("firstName", firstName);
+        req.setAttribute("lastName", lastName);
+        req.getRequestDispatcher("/home.jsp").forward(req, resp);
+    }
+}
---
+++ src/main/webapp/Register-Success.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: hp
+  Date: 5/7/2022
+  Time: 4:53 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>Register Success</h1>
+<p><%=request.getAttribute("username")%></p>
+<p><%=request.getAttribute("password")%></p>
+</body>
+</html>
